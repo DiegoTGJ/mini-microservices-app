cf19629d93bb01d436dadc0e24ceffe6a74d22f1
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-29 02:16:43 -0400

    Added client components

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const posts = {};
 
