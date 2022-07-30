4c8a6f3356578499c78ec5a0a933bef11524eec3
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-30 12:17:56 -0400

    Added ingress controler, Skaffold config

--- posts/index.js
+++ posts/index.js
@@ -12,11 +12,7 @@ const port = process.env.PORT || 4000;
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -36,7 +32,7 @@ app.post("/posts", async (req, res) => {
 
 app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
-
+  console.log("something");
   res.send({});
 });
 
